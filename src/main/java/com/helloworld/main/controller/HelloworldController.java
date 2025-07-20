18f589281c0814afe17843038eddfc40ae3176fa
Kishore25Git <akishore25@gmail.com>
2025-07-20 23:36:13 +0800

    recommit

--- src/main/java/com/helloworld/main/controller/HelloworldController.java
+++ src/main/java/com/helloworld/main/controller/HelloworldController.java
@@ -1,4 +1,4 @@
-package com.helloworld.main;
+package com.helloworld.main.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
